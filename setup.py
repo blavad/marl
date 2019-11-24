3dca7cc06fe79d6ec9b0e46f53e4071389ef68b4
davalb <david.albert@insa-rouen.fr>
2019-11-24 11:04:04 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='marl',
+      version='0.0.1',
+      description='Multi-Agent Reinforcement Learning',
+      url='https://github.com/blavad/marl',
+      author='David Albert',
+      author_email='david.albert@insa-rouen.fr',
+      packages=find_packages(),
+      install_requires=['gym', 'numpy']
+)
