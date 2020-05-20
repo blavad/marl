1b82a648a46b3b10f07e9a159145cd2df6f530a8
David Albert <david.albert@insa-rouen.fr>
2020-05-20 15:23:21 +0200

    Add UCB1 exploration

--- marl/exploration/expls.py
+++ marl/exploration/expls.py
@@ -9,16 +9,19 @@ class UCB1(ExplorationProcess):
     def __init__(self, n_actions):
         self.n_actions = n_actions
         self.count = [1 for _ in range(n_actions)]
+        self.t = sum(self.count)
 
     def reset(self, t=None):
         """ Reinitialize the state of the process """
         self.count = [1 for _ in range(self.n_actions)]         
+        self.t = sum(self.count)
         
     def update(self, t):
-        pass
+        self.t = self.t + 1
             
     def __call__(self, policy, observation):
-        value = policy.model(observation) + torch.tensor([math.sqrt(2*math.log(100)/i) for i in self.count])
-        action = value.argmax().item()
+        q_value = policy.model(observation)
+        ucb1_value = q_value + q_value.max().item() * torch.tensor([math.sqrt(2*math.log(self.t)/i) for i in self.count])
+        action = ucb1_value.argmax().item()
         self.count[action] += 1
         return action
